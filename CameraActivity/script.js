b357370249a22b1d6da67077cf3aa1739b88d67a
lakshita15 <lakshitamahajan2001@gmail.com>
2021-05-30 13:51:22 +0530

    Camera LeCTURE3

--- CameraActivity/script.js
+++ CameraActivity/script.js
@@ -1,13 +1,45 @@
 let video = document.querySelector("video");
 let vidBtn = document.querySelector("button#record");
-
+let body = document.querySelector("body")
 let capBtn = document.querySelector("button#capture");
-
-let constraints = { video: true, audio: true };
+let filters = document.querySelectorAll(".filter")
+let zoomin = document.querySelector(".zoom-in");
+let zoomout = document.querySelector(".zoom-out")
+let constraints = { video: true, audio: false };
 let mediaRecorder;
 let isRecording = false;
 let chunks = [];
 
+let minzoom = 1;
+let maxzoom = 3;
+let currzoom = 1;
+let filter = "";
+
+for(let i =0 ; i < filters.length ; i++){
+  filters[i].addEventListener("click", function(e){
+
+    filter = e.currentTarget.style.backgroundColor;
+    removefilter();
+    applyfilter(filter);
+
+  })
+}
+zoomin.addEventListener("click" , function(){
+ let videocurrscale = video.style.transform.split("(")[1].split(")")[0];
+if(videocurrscale > maxzoom){
+  return;
+}else{
+  currzoom = Number(videocurrscale) + 0.1;
+  video.style.transform = `scale(${currzoom})`;
+}
+});
+zoomout.addEventListener("click" , function(){
+ 
+ if(currzoom > minzoom){
+  currzoom-= 0.1;
+  video.style.transform = `scale(${currzoom})`;
+ }
+ })
 vidBtn.addEventListener("click", function () {
   let innerDiv = vidBtn.querySelector("div");
 
@@ -17,6 +49,8 @@ vidBtn.addEventListener("click", function () {
     innerDiv.classList.remove("record-animation");
   } else {
     mediaRecorder.start();
+    filter=""
+    removefilter();
     isRecording = true;
     innerDiv.classList.add("record-animation");
   }
@@ -60,10 +94,30 @@ function capture() {
   c.width = video.videoWidth;
   c.height = video.videoHeight;
   let ctx = c.getContext("2d");
+
+  ctx.translate(c.width/2 , c.height/2);
+  ctx.scale(currzoom , currzoom);
+  ctx.translate(-c.width/2 , -c.height/2);
+
   ctx.drawImage(video, 0, 0);
+  if(filter!= ""){
+    ctx.fillStyle=filter;
+    ctx.fillRect(0,0,c.width , c.height)
+  }
   let a = document.createElement("a");
   a.download = "image.jpg";
   a.href = c.toDataURL();
   a.click();
   a.remove();
+}
+function applyfilter(filterColor){
+let filterdiv= document.createElement("div")
+filterdiv.classList.add("filter-div");
+filterdiv.style.backgroundColor = filterColor;
+body.appendChild(filterdiv)
+}
+function removefilter(){
+  let filterdiv = document.querySelector(".filter-div")
+if(filterdiv) filterdiv.remove()
+
 }
