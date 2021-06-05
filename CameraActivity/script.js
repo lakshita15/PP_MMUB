3faaa9a85efc1a20bdbef22b3e201d8b53bd8413
lakshita15 <lakshitamahajan2001@gmail.com>
2021-06-05 13:49:40 +0530

    code updated

--- CameraActivity/script.js
+++ CameraActivity/script.js
@@ -76,7 +76,7 @@ navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
 
   mediaRecorder.addEventListener("stop", function () {
     let blob = new Blob(chunks, { type: "video/mp4" });
-
+    addMedia("video" , blob)
     chunks = [];
 
     let url = URL.createObjectURL(blob);
@@ -107,6 +107,7 @@ function capture() {
   let a = document.createElement("a");
   a.download = "image.jpg";
   a.href = c.toDataURL();
+  addMedia("img" , c.toDataURL())
   a.click();
   a.remove();
 }
