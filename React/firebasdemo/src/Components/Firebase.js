f20ea4674a3bc2602f731c0d7c340ecfbdaf2e17
lakshita15 <lakshitamahajan2001@gmail.com>
2021-07-04 20:12:49 +0530

    ,,

---
+++ React/firebasdemo/src/Components/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+firebase.initializeApp(
+
+{
+    apiKey: "AIzaSyDa02ZXh1sx2uG43IFX9wFj_EElJf7pgBE",
+    authDomain: "class-app-340d9.firebaseapp.com",
+    projectId: "class-app-340d9",
+    storageBucket: "class-app-340d9.appspot.com",
+    messagingSenderId: "296706696268",
+    appId: "1:296706696268:web:79e161c034e530248100e9"
+  });
+  export default firebase;
+ 
